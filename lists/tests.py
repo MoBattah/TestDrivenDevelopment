cdc1f8d9b8474092f06525610671afcba995dff2
MoBattah <devbattah@gmail.com>
2020-04-19 15:02:42 -0500

    Added home.html assertion

--- lists/tests.py
+++ lists/tests.py
@@ -14,3 +14,8 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do Lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+        with open('lists/templates/home.html') as f:
+            expected_content = f.read()
+
+        self.assertEqual(response.content.decode(), expected_content)
