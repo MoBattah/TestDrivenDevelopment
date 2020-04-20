107f607e58931c788366e314bc110bbb7ec14de9
MoBattah <devbattah@gmail.com>
2020-04-19 21:57:22 -0500

    Added second test for post request functionality

--- lists/tests.py
+++ lists/tests.py
@@ -8,8 +8,14 @@ class HomePageTest(TestCase):
 
     def test_home_page_is_about_to_do_lists(self):
         request = HttpRequest()
+        response = home_page(request)
+        self.assertIn("to-do item", response.content.decode())
+
+    def test_home_page_can_remember_post_requests(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new item'
 
         response = home_page(request)
 
-        expected_content = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_content)
+        self.assertIn('A new item', response.content.decode())
